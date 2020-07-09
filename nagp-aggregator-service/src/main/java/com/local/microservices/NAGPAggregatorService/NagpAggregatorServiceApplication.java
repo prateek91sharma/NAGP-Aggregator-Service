84d2868262241b63417bb9366f348c4e70fa80b6
Prateek Sharma <prateek.sharma02@nagarro.com>
2020-07-09 13:27:48 +0530

    Initial Commit

---
+++ nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/NagpAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.local.microservices.NAGPAggregatorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagpAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagpAggregatorServiceApplication.class, args);
+	}
+
+}
---
+++ nagp-aggregator-service/src/test/java/com/local/microservices/NAGPAggregatorService/NagpAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.microservices.NAGPAggregatorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagpAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
