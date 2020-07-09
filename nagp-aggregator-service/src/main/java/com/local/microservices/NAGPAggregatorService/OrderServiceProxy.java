a6850304394f41b3bf71505ec3041615a1f03d82
Prateek Sharma <prateek.sharma02@nagarro.com>
2020-07-10 02:11:29 +0530

    Changes due to Ribbon issue

--- nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/OrderServiceProxy.java
+++ nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/OrderServiceProxy.java
@@ -3,11 +3,13 @@ package com.local.microservices.NAGPAggregatorService;
 import java.text.ParseException;
 import java.util.List;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(/* name = "${order.service.url:orders-service}", */ value="orders-service" /*, url="${order.service.url}"*/)
+@RibbonClient("orders-service")
 public interface OrderServiceProxy {
 	
 	@GetMapping("/orders/{userId}")
--- nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/UserServiceProxy.java
+++ nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/UserServiceProxy.java
@@ -1,10 +1,12 @@
 package com.local.microservices.NAGPAggregatorService;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(/* name = "${user.service.url:user-details}", */ value="user-details"/*, url="${user.service.url}"*/)
+@RibbonClient("user-details")
 public interface UserServiceProxy {
 	
 	@GetMapping("/user/{id}")
