b902e550baf429f2819278b60a828fa37faa520c
Prateek Sharma <prateek.sharma02@nagarro.com>
2020-07-10 13:53:49 +0530

    Feign client workaround

--- nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/OrderServiceProxy.java
+++ nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/OrderServiceProxy.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(/* name = "${order.service.url:orders-service}", */ value="orders-service" /*, url="${order.service.url}"*/)
 @RibbonClient("orders-service")
+@Component
+@Scope("prototype")
 public interface OrderServiceProxy {
 	
 	@GetMapping("/orders/{userId}")
--- nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/UserServiceProxy.java
+++ nagp-aggregator-service/src/main/java/com/local/microservices/NAGPAggregatorService/UserServiceProxy.java
@@ -2,11 +2,15 @@ package com.local.microservices.NAGPAggregatorService;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(/* name = "${user.service.url:user-details}", */ value="user-details"/*, url="${user.service.url}"*/)
 @RibbonClient("user-details")
+@Component
+@Scope("prototype")
 public interface UserServiceProxy {
 	
 	@GetMapping("/user/{id}")
